3c09307a9dfe302b57c458c9e342f997975e2d77
xiachedan99 <139933067+xiachedan99@users.noreply.github.com>
2023-08-08 05:17:48 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,45 +5,40 @@
 #                                               #
 #################################################
 
-# ANSI color codes
-green='\033[32m'
-plain='\033[0m'
-
-# Install required packages
+#install base
 rpm -q curl || yum install -y curl
 rpm -q wget || yum install -y wget
 apt-get install git -y || yum install git -y
 
-# Install v2fly
+#install v2fly
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 
-# Configure info
+#Config info
 cd /usr/local/etc/v2ray && rm -rf config.json
 wget https://raw.githubusercontent.com/xiachedan99/v2Foward/main/config.json
 
-# Set configuration
-read -p "${green}请输入监听端口：${plain}" listen_port
-read -p "${green}请输入被转发端口（落地机端口）：${plain}" into_port
-read -p "${green}请输入被转发IP（落地机IP）：${plain}" into_address
+#setting config
+read -p "请输入监听端口：" listen_port
+read -p "请输入被转发端口（落地机端口）：" into_port
+read -p "请输入被转发IP（落地机IP）：" into_address
 
-# Update configuration
+#written config
 file="/usr/local/etc/v2ray/config.json"
 sed -i "s|Nat_port|${listen_port}|" $file
 sed -i "s|Ld_ipadress|${into_address}|" $file
 sed -i "s|Ld_port|${into_port}|" $file
 
-# Enable and start v2ray
+#system 
 systemctl enable v2ray
 systemctl start v2ray
 service v2ray restart
 service v2ray status
+echo -e "\033[32m 安装完成 \033[0m"
+echo -e ""
+echo -e " 重启v2ray转发： service v2ray restart"
+echo -e " 查看v2ray转发： service v2ray status"
+echo -e " 移除v2ray请运行： bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove"
+echo -e ""
+echo -e " 配置多用户转发，请至 \033[32m/usr/local/etc/v2ray\033[0m 修改config.json配置文件"
 
-# Print completion message
-echo -e "${green}安装完成${plain}"
-echo -e ""
-echo -e "重启v2ray转发： service v2ray restart"
-echo -e "查看v2ray转发： service v2ray status"
-echo -e "移除v2ray转发请运行： bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --remove"
-echo -e ""
-echo -e "配置多用户转发，请至文件目录 /usr/local/etc/v2ray 修改config.json配置文件"
 echo -e ""
