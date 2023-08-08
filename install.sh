0b847625b1c96696f7d8752dd74361136ddc72c3
xiachedan99 <139933067+xiachedan99@users.noreply.github.com>
2023-08-08 04:17:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,9 +19,9 @@ cd /usr/local/etc/v2ray && rm -rf config.json
 wget https://raw.githubusercontent.com/xiachedan99/v2Foward/main/config.json
 
 #setting config
-read -p "请输入监听端口：" listen_port
-read -p "请输入被转发端口：" into_port
-read -p "请输入被转发IP：" into_address
+read -p "\033[32m 请输入监听端口： \033[0m" listen_port
+read -p "\033[32m 请输入被转发端口（落地机端口）：\033[0m" into_port
+read -p "\033[32m请输入被转发IP（落地机IP）：\033[0m" into_address
 
 #written config
 file="/usr/local/etc/v2ray/config.json"
@@ -35,3 +35,6 @@ systemctl start v2ray
 service v2ray restart
 service v2ray status
 echo -e "\033[32m 安装完成 \033[0m"
+echo -e "service v2ray restart \033[32m 重启v2ray \033[0m"
+echo -e "service v2ray status \033[32m 查看v2ray \033[0m"
+echo -e "配置多用户转发，请至/usr/local/etc/v2ray/config.json修改配置文件"
